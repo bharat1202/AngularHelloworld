d159a01c0b9995012679ade063577a43f42c8910
bharat1202 <152322224+bharat1202@users.noreply.github.com>
2025-03-24 15:46:18 +0530

    Logo connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hello from BridgeLabz';
-  logoPath = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbqt1bChQ8i-BfxpDMol6aBwJkkJtdR4Vv1A&s';  // Property for image binding
+  logoPath = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQbqt1bChQ8i-BfxpDMol6aBwJkkJtdR4Vv1A&s';  
+
+  // Method to open BridgeLabz site in a new tab
+  openBridgeLabzSite() {
+    window.open('https://www.bridgelabz.com/', '_blank');
+  }
 }
